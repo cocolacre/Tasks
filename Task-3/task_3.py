5b491bc9677996aca18bfcae6b48daa7c52426da
cocolacre <cocolacre@gmail.com>
2019-06-18 22:23:10 +0300

    Task #3

---
+++ Task-3/task_3.py
@@ -0,0 +1,4 @@
+#  for each half-hour interval we summarize 
+# values from every cashbox.
+# Let's say files for cashboxes are called 1.txt, 2.txt and so on.
+files = ['1.txt', '2.txt','3.txt','4.txt','5.txt']
