b1979fd9664203529af8fe51ce043d56a8e4a309
cocolacre <cocolacre@gmail.com>
2019-06-18 23:40:47 +0300

    Task #3 alpha

--- Task-3/task_3.py
+++ Task-3/task_3.py
@@ -1,4 +1,19 @@
+import numpy as np
+
 #  for each half-hour interval we summarize 
 # values from every cashbox.
-# Let's say files for cashboxes are called 1.txt, 2.txt and so on.
+
+# Let's assume files for cashboxes are called 1.txt, 2.txt and so on.
 files = ['1.txt', '2.txt','3.txt','4.txt','5.txt']
+data = []
+for fl in files:
+    with open(fl, 'r') as f:
+        values = [int(x[:-1]) for x in f.readlines()]
+        data.append(values)
+sums = []
+for ind, value in enumerate(data[0]):
+    sums.append(data[0][ind]+data[1][ind]+data[2][ind]+data[3][ind]+data[4][ind])
+#print(np.array(sums).amax())
+m = max(sums)
+max_intervals = [i for i, j in enumerate(sums) if j == m]
+print(str(m)+ ': ' + str(max_intervals))
