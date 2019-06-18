6c84c83667df568e97c5dc746482afb9eeb252f4
cocolacre <cocolacre@gmail.com>
2019-06-18 21:56:32 +0300

    Added some comments

--- task_1.py
+++ task_1.py
@@ -1,5 +1,8 @@
 import sys, os
+data = []
 
+
+#if there is no numpy module installed - let's try installing it.
 try:
     import numpy as np
 except Exception as _e:
@@ -10,24 +13,28 @@ try:
     import numpy as np
 except Exception as _e:
     print(str(_e))
+    #we could not install or import numpy. So - exiting.
     sys.exit()
 
-#load data
-data = []
-if len(sys.argv) == 1:
+#if we do not pass data file as argument - let's try and read data.txt
+#within the same folder
+if len(sys.argv) == 1: 
     with open('data.txt', 'r') as f:
         data = f.readlines()
     data = np.array([int(item[:-1]) for item in data])
-    #print(data)
+
+#if we pass data file as argument:
 elif len(sys.argv) == 2:
     datafile = sys.argv[1]
     with open(datafile, 'r') as f:
         data = f.readlines()
     data = np.array([int(item[:-1]) for item in data])
+#if there are too many arguments passed - exit.
 else:
     print('Too many arguments.')
     sys.exit()
-# 90 percentile
+
+#make desired output:
 print('90 percentile ' + str(np.percentile(data,90)))
 print('median ' + str(np.percentile(data,50)))
 print('average ' + str(np.average(data)))
