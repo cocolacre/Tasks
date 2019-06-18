875406713b3b38f5535a6b242ddd7cae30b94381
cocolacre <cocolacre@gmail.com>
2019-06-18 22:00:26 +0300

    Generate numbers for Task #1

--- generate_numbers.py
+++ generate_numbers.py
@@ -1,7 +1,8 @@
-#generate numbers 
+#generate numbers for the first task.
+
 import random
-N = 100
-MAX = 9999
+N = 100 #number of numbers to generate.
+MAX = 9999 #maximum number.
 with open('data.txt', 'a+') as f:
     for i in range(N):
         f.write(str(random.randint(0,MAX)) + '\n')
