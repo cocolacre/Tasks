7e9343e0476bd1c1ae5b7a14b142252ddd313c27
cocolacre <cocolacre@gmail.com>
2019-06-19 20:38:40 +0300

    Done with task # 2

--- Task-2/task_2.py
+++ Task-2/task_2.py
@@ -2,19 +2,61 @@ import sys
 
 
 #read polygon data
-print(len(sys.argv))
+#print(len(sys.argv))
 if len(sys.argv) == 1:
     fname = 'data.txt'
 elif len(sys.argv) == 2:
     fname = sys.argv[1]
-print(fname)
+#print(fname)
 
 
 poly = []
+def point_within_polygon(x, y, poly, include_edges=True):
+
+    n = len(poly)
+    inside = False
+    for point in poly:
+        if point == [x,y]:
+            msg = "Точка - вершина четырехугольника"
+            return msg
+        
+    p1x, p1y = poly[0]
+    for i in range(1, n + 1):
+        p2x, p2y = poly[i % n]
+        if p1y == p2y:
+            if y == p1y:
+                if min(p1x, p2x) <= x <= max(p1x, p2x):
+                    msg = "Точка лежит на сторонах четырехугольника"
+                    inside = include_edges
+                    return msg
+                    break
+                elif x < min(p1x, p2x):
+                    inside = not inside
+        else:  # p1y!= p2y
+            if min(p1y, p2y) <= y <= max(p1y, p2y):
+                xinters = (y - p1y) * (p2x - p1x) / float(p2y - p1y) + p1x
+
+                if x == xinters:
+                    inside = include_edges
+                    msg = "Точка лежит на сторонах четырехугольника"
+                    return msg
+                    break
+                if x < xinters:
+                    inside = not inside
+        p1x, p1y = p2x, p2y
+    if inside:
+        msg = 'точка внутри четырехугольника'
+    else:
+        msg = 'точка снаружи четырехугольника'
+    return msg
+
 with open(fname, 'r') as f:
     for line in f.readlines():
         poly.append([float(item) for item in line.split(' ')])
-print(poly)
+#print(poly)
 
 x = float(input('X: '))
 y = float(input('Y: '))
+
+res = point_within_polygon(x,y,poly)
+print(res)
