984e2ee354bb7ffb7daec4f22d64b2a2357bb17f
cocolacre <cocolacre@gmail.com>
2019-06-19 20:15:56 +0300

    Working on task 2

--- Task-2/task_2.py
+++ Task-2/task_2.py
@@ -1,7 +1,20 @@
+import sys
 
 
+#read polygon data
+print(len(sys.argv))
+if len(sys.argv) == 1:
+    fname = 'data.txt'
+elif len(sys.argv) == 2:
+    fname = sys.argv[1]
+print(fname)
 
 
+poly = []
+with open(fname, 'r') as f:
+    for line in f.readlines():
+        poly.append([float(item) for item in line.split(' ')])
+print(poly)
 
 x = float(input('X: '))
 y = float(input('Y: '))
